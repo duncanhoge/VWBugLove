e28a7457424adf8d08715676babf54cb7657775a
Duncan Hoge <duncan.hoge@gmail.com>
2014-09-25 18:47:58 -0400

    It must be uniquePress
    Cleaned up code. 2nd LED still stays on unless I push button twice,
    then it turns off. Mst be something to do with uniquePress?

--- VWBugLove.ino
+++ VWBugLove.ino
@@ -3,9 +3,6 @@
 #include <OneSheeld.h>
 #include <Button.h>
 
-
-
-
 /* Define LEDs */
 int firstLED = 7;
 int secondLED = 8;
@@ -17,7 +14,7 @@ Button button = Button(12,PULLUP);
 /* Timer, to fade LEDs*/
 long previousMillis = 0;//last time LED updated
 long interval = 5000; //time til fade
-int lastLED = 0;//define last LED > target of fade, will change
+byte lastLED = 0;//define last LED > target of fade, will change
 
 void setup()
 { 
@@ -29,15 +26,8 @@ void setup()
   pinMode(thirdLED,OUTPUT);
 }
 
-/*IMPORTANT:
-Make sure to set OneSheeld input to buttoninput (12);
-*/
-
-byte pressCount = 0;
+byte pressCount = 1;
 void loop(){
-
-  //start timer
-  unsigned long currentMillis = millis();
   
   //button functionality for LED scale
   if (button.uniquePress())  
@@ -53,54 +43,46 @@ void loop(){
           case 1:
           
             digitalWrite(firstLED,HIGH);
-            digitalWrite(thirdLED,LOW);
             digitalWrite(secondLED,LOW);
-            lastLED=1;
+            digitalWrite(thirdLED,LOW);
+            lastLED=firstLED;
             pressCount++;
   
           break;
           case 2:
-  
-            digitalWrite(secondLED,HIGH);
+            
             digitalWrite(firstLED,HIGH);
-            digitalWrite(6,LOW);
+            digitalWrite(secondLED,HIGH);
+            digitalWrite(thirdLED,LOW);
             lastLED=secondLED;
-            lastLED=2;
             pressCount++;
    
           break;
           case 3:
     
-            digitalWrite(thirdLED,HIGH);
-            digitalWrite(secondLED,HIGH);
             digitalWrite(firstLED,HIGH);
+            digitalWrite(secondLED,HIGH);
+            digitalWrite(thirdLED,HIGH);
             lastLED=thirdLED;
-            lastLED=3;
-            pressCount++;
-        
-          break;
-          case 4:
-          
-            digitalWrite(firstLED,LOW);
-            digitalWrite(thirdLED,LOW);
-            digitalWrite(secondLED,LOW);
-            lastLED=0;
-            pressCount = 1;
+            pressCount=3;
             
           break;  
 
         }
 
-//  //fade timer
-//  if(currentMillis - previousMillis > interval) {
-//    // save the last time you blinked the LED 
-//    previousMillis = currentMillis;
-//    //turn off last led
-//    digitalWrite(lastLED, LOW);
-//      if(pressCount>0){
-//        pressCount--;
-//      }
-//  }  
+  //start timer
+  unsigned long currentMillis = millis();
+
+  //fade timer
+  if(currentMillis - previousMillis > interval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;
+    //turn off last led
+    digitalWrite(lastLED, LOW);
+      if(pressCount>0){
+        pressCount--;
+      }
+  }  
 
 }
 
