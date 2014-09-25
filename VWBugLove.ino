64ac7a8e48ccbe3c7ecec22119aa6e75fbee7bfb
Duncan Hoge <duncan.hoge@gmail.com>
2014-09-25 16:47:43 -0400

    Revert "LED Scale Working, Physical Button"
    This reverts commit 3d0c413be1ce39bb71218d8a498babbcd4c10f9e.

--- VWBugLove.ino
+++ VWBugLove.ino
@@ -1,70 +1,33 @@
 /* Include 1Sheeld library. */
 #include <OneSheeld.h>
-#include <Button.h>
 
-
-/* Define LEDs */
-int firstLED = 7;
-int secondLED = 8;
-int thirdLED = 9;
-
-/* Physical Button */
-//hopefully can get OneSheeld to do this
-Button button = Button(12,PULLUP);
+/* A name for the LED on pin 13. */
+int firstLED = 13;
+int secondLED = 12;
 
 void setup()
 {
   /* Start communication. */
-  OneSheeld.begin(); //currently not needed.
+  OneSheeld.begin();
   /* Set the LED pin as output. */
   pinMode(firstLED,OUTPUT);
   pinMode(secondLED,OUTPUT);
-  pinMode(thirdLED,OUTPUT);
 }
 
-/*GOAL: toggle light on and off, esp in succession for two leds*/
-/*ToggleButton may be the way to go.*/
-
-byte pressCount = 0;
-void loop(){
-
-  if (button.uniquePress())  
-    
-    pressCount++;
-  
-    switch(pressCount)
-        
-        { case 1:
-          
-            digitalWrite(firstLED,HIGH);
-            digitalWrite(thirdLED,LOW);
-            digitalWrite(secondLED,LOW);
-  
-          break;
-          case 2:
-  
-            digitalWrite(secondLED,HIGH);
-            digitalWrite(firstLED,HIGH);
-            digitalWrite(6,LOW);
-   
-          break;
-          case 3:
-    
-            digitalWrite(thirdLED,HIGH);
-            digitalWrite(secondLED,HIGH);
-            digitalWrite(firstLED,HIGH);
-        
-          break;
-          case 4:
-          
-            digitalWrite(firstLED,LOW);
-            digitalWrite(thirdLED,LOW);
-            digitalWrite(secondLED,LOW);
-       
-          pressCount = 0;
-          break;  
-
+void loop()
+{
+  /* Always check if the push button is pressed. */
+  if(PushButton.isPressed())
+  {
+    /* Turn on the LED. */
+    digitalWrite(firstLED,HIGH);
+    digitalWrite(secondLED,HIGH);
+  }
+  else
+  {
+    /* Turn on the LED. */
+    digitalWrite(firstLED,LOW);
+    digitalWrite(secondLED,LOW);
   }
 
- 
-   }
+}
